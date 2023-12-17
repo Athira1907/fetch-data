5ae315437599b435097ded55f3c16905e47a1b3d
Athira1907 <athiraanilputhenparampil@gmail.com>
2023-12-17 12:01:30 +0530

    some changes

--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -1,11 +1,31 @@
-import React, { useState } from 'react'
+import React, { useState ,useEffect}  from 'react'
+import axios from 'axios'
 import './style.css'
 function Register() {
     const [name,setName]=useState('')
     const [email,setEmail]=useState('')
     const [password,setPassword]=useState('')
     const [mobile,setMobile]=useState('')
+    useEffect(()=>{
+      axios.get('http://localhost:8000/userdetails')
+      .then(users=>{
+      //setUsers(users.data)
+      console.log(users.data);
+      console.log(typeof(users.data));
+      console.log(users.data['name']);
+      var namedetail=users.map(item=>item.name)
+      console.log('name is ',namedetail);
+      
+      }).catch(err=>{
+          // "Request failed with status code 404" error, 
+          //it means that the server is unable to find the resource you are trying to access.
+          console.log('error',err);
+      })
+          },[])
+const handleSubmit=(e)=>{
+e.preventDefault()
 
+}
   return (
     <>
     <div className="container">
@@ -27,7 +47,7 @@ function Register() {
             <label htmlFor="">Mobile</label>
             <input type="text" onChange={(e)=>setMobile(e.target.value)} className='input'/>
             </div>
-            <butto className='btn'>Register</butto>
+            <butto className='btn' onclick={handleSubmit}>Register</butto>
         </form>
     </div>
     
